f67e372ebc4546783ab811f91ba00b693b887e4f
Faleous <myfirsttwink1@gmail.com>
2022-07-02 14:28:50 +0300

    base controller, logger'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { App } from "./app";
+import { LoggerService } from "./logger/logger.service";
 
 
 async function bootstrap() {
-    const app = new App();
+    const app = new App(new LoggerService());
     await app.initApp();
 }
 
