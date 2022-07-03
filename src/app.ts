d12b38f516c05b6ac2629829c3d52d9e1afa8a2e
Faleous <myfirsttwink1@gmail.com>
2022-07-03 14:41:15 +0300

    decorators, errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
 import { Server } from 'http';
+import { ExceptionFilter } from "./errors/exception.filter";
+import { ILogger } from "./logger/logger.interface";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.contorller";
 
@@ -7,26 +9,34 @@ export class App {
     app: Express;
     server: Server;
     port: number;
-    logger: LoggerService;
+    logger: ILogger;
     userContorller: UserController;
+    exceptionFilter: ExceptionFilter;
 
 
     constructor(
-        logger: LoggerService,
-        userController: UserController
+        logger: ILogger,
+        userController: UserController,
+        exceptionFilter: ExceptionFilter
     ) {
         this.app = express();
         this.port = 8000;
         this.logger = logger;
         this.userContorller = userController;
+        this.exceptionFilter = exceptionFilter;
     }
 
     useRoutes() {
         this.app.use('/users', this.userContorller.router);
     }
 
+    useExceptionFilters() {
+        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+    }
+
     public async initApp() {
         this.useRoutes();
+        this.useExceptionFilters();
         this.server = this.app.listen(this.port);
         this.logger.log(`Server started on http://loclahost:${this.port}`)
     }
--- src/users/users.contorller.ts
+++ src/users/users.contorller.ts
@@ -2,6 +2,7 @@ import { BaseController } from "../common/base.controller";
 import { IControllerRoute } from "../common/route.interface";
 import { LoggerService } from "../logger/logger.service";
 import { Request, Response, NextFunction } from "express";
+import { HTTPError } from "../errors/http-error.class";
 
 export class UserController extends BaseController {
     constructor(
@@ -18,9 +19,9 @@ export class UserController extends BaseController {
     ];
 
     login(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'login');
+        next(new HTTPError(401, "cannot authorize", 'login'));
     }
-
+    
     register(req: Request, res: Response, next: NextFunction) {
         this.ok(res, 'register');
     }
